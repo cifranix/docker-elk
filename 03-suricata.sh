4b82fd20cc3949896796bbabb20898bd639cace9
Your Name <you@example.com>
2020-06-10 15:58:50 -0700

    adding suricata files

--- 03-suricata.sh
+++ 03-suricata.sh
@@ -2,6 +2,7 @@ docker rm -f suricata
 docker run --name suricata -d --net=host \
   -v /root/docker-elk/suricata/logs/:/var/log/suricata/ \
   -v /root/docker-elk/suricata/etc/:/etc/suricata/ \
+  -v /root/docker-elk/suricata/rules/suricata.rules:/var/lib/suricata/rules/suricata.rules \
   -e INT=${interface} \
   docker.io/cifranix/suricata:4.1.2 \
   bash -c "echo ${INT} && /usr/bin/suricata -c /etc/suricata/suricata.yaml -i ${interface} --init-errors-fatal"
